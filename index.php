2be969941191a0ae832f81bc33502ea607a02073
Gaikko <Gaikko@gmail.com>
2022-10-03 11:23:40 -0400

    css

--- index.php
+++ index.php
@@ -12,6 +12,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inicio - Hub Inovation</title>
+    <link rel="stylesheet" href="./css/style.css">
 </head>
 <body>
     <h1><?php print($_msg) ?></h1>
